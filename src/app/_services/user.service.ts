1542fee5020aba2c42d3b62df576eba7304f430f
Netpoc <ayenotopeter@hotmail.com>
2023-03-17 17:17:10 +0000

    init

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
     constructor(private http: HttpClient) { }
 
     getAllProducts() {
-        return this.http.get(`http://localhost:5050/api/getfarmer`);
+        return this.http.get(`https://nakore.onrender.com/api/getfarmer`);
     }
 
     getAllFarmers() {
-        return this.http.get(`http://localhost:5050/api/getfarmer`);
+        return this.http.get(`https://nakore.onrender.com/api/getfarmer`);
     }
 }
